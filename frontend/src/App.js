ed95b803d62a742aaa8e5fe59240c721d599d1b7
Syllas <syllasbraga2@gmail.com>
2024-12-01 15:06:50 -0300

    feat(frontend): Criação das páginas de conta e movimentacao e rotas para as três

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PessoaPage from "./pages/PessoaPage";
+import ContaPage from "./pages/ContaPage";
+import MovimentacaoPage from "./pages/MovimentacaoPage";
 
-const App = () => <PessoaPage />;
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/pessoa-page" element={<PessoaPage />} />
+        <Route path="/conta-page" element={<ContaPage />} />
+        <Route path="/movimentacao-page" element={<MovimentacaoPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ frontend/src/pages/MovimentacaoPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovimentacaoPage = () => {
+  return <h1>Movimentação Page</h1>;
+};
+
+export default MovimentacaoPage;
